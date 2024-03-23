ef317b2081f01dc98193b2f9634d25e3e8a334c0
RankJay <rankjay0101@gmail.com>
2024-03-23 16:13:31 +0530

    using frames.js for rendering

--- ritual-frame-frontend/src/app/frame/page.tsx
+++ ritual-frame-frontend/src/app/frame/page.tsx
@@ -1,17 +1,25 @@
-import { getFrameMetadata } from "@coinbase/onchainkit/frame";
+import {
+  FrameButton,
+  FrameContainer,
+  FrameImage,
+  FrameReducer,
+  getPreviousFrame,
+  useFramesReducer,
+  validateActionSignature,
+} from "frames.js/next/server";
 import type { Metadata } from "next";
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: "Convince Ritual AI to mint NFT!",
-    },
-  ],
-  image: {
-    src: `${process.env.NEXT_PUBLIC_SITE_URL}/assets/get-started.jpg`,
-  },
-  postUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/api/start`,
-});
+// const frameMetadata = getFrameMetadata({
+//   buttons: [
+//     {
+//       label: "Convince Ritual AI to mint NFT!",
+//     },
+//   ],
+//   image: {
+//     src: `${process.env.NEXT_PUBLIC_SITE_URL}/assets/get-started.jpg`,
+//   },
+//   postUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/api/start`,
+// });
 
 export const metadata: Metadata = {
   title: "Advanced Frame",
@@ -21,15 +29,34 @@ export const metadata: Metadata = {
     description: "Another, more advanced frame example",
     images: [`${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`],
   },
-  other: {
-    ...frameMetadata,
-  },
+  // other: {
+  //   ...frameMetadata,
+  // },
 };
 
-export default function Page() {
+const reducer: FrameReducer = (state, action) => ({});
+
+export default async function Page(props: any) {
+  const previousFrame = getPreviousFrame(props.searchParams);
+  await validateActionSignature(previousFrame.postBody);
+  const [state, dispatch] = useFramesReducer(
+    reducer,
+    { count: 0 },
+    previousFrame
+  );
+
   return (
-    <>
-      <h1>Advanced Frame</h1>
-    </>
+    <FrameContainer
+      postUrl="/api/start"
+      pathname={`${process.env.NEXT_PUBLIC_SITE_URL}`}
+      state={state}
+      previousFrame={previousFrame}
+    >
+      <FrameImage
+        src={`${process.env.NEXT_PUBLIC_SITE_URL}/assets/get-started.jpg`}
+      />
+      {/* <FrameInput text="NA" /> */}
+      <FrameButton>Convince Ritual AI to mint NFT!</FrameButton>
+    </FrameContainer>
   );
 }
