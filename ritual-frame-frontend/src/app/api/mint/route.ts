8214e9e139f5b0c3fab081b43ae0ac5660b564ad
RankJay <rankjay0101@gmail.com>
2024-03-24 21:28:56 +0530

    airstack valdiation

--- ritual-frame-frontend/src/app/api/mint/route.ts
+++ ritual-frame-frontend/src/app/api/mint/route.ts
@@ -1,8 +1,22 @@
 import { generateFrameHTML } from "@/actions";
+import { init } from "@airstack/airstack-react";
+import { validateFramesMessage } from "@airstack/frames";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
+  init(process.env.AIRSTACK_API_KEY ?? "");
   const body = await req.json();
+  const { isValid } = await validateFramesMessage(body);
+
+  if (!isValid) {
+    const searchParams = new URLSearchParams({
+      prompt: "You have not been granted an NFT.",
+    });return new NextResponse(
+      generateFrameHTML({
+        imageSrc: `${process.env.NEXT_PUBLIC_SITE_URL}/og?${searchParams}`,
+      })
+    );
+  }
 
   const { addr } = body;
   const searchParams = new URLSearchParams({
